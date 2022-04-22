32c6865b3469c4bd98edf5942fe76c12e2f4d76b
Duong Manh <manhme2953@gmail.com>
2022-04-22 22:07:13 +0700

    all

---
+++ main.js
@@ -0,0 +1,45 @@
+const wrapper = document.querySelector(".wrapper");
+var inputWord = wrapper.querySelector(".input");
+var showInfor = wrapper.querySelector(".word .detail")
+var langs = wrapper.querySelectorAll(".language input")
+var langscontainer = wrapper.querySelector(".language")
+
+
+
+function data(result) {
+    wrapper.classList.add("active")
+    showInfor.innerHTML = `${result[0][0][0]}`;
+}
+
+
+
+// langs.forEach(lang => {
+//     lang.addEventListener("click", e => {
+//         if (lang.checked === true) {
+//             langchoosed = lang.getAttribute("id");
+//             console.log(langchoosed)
+//         }
+//     })
+// })
+
+inputWord.addEventListener("keyup", e => {
+    inputWord.value = e.target.value.replace(/""/g, ",")
+    if (inputWord.focus) {
+        wrapper.classList.remove("active")
+    }
+    if (e.key === "Enter" && inputWord.value) {
+        fetchAPI(inputWord.value)
+    }
+
+
+})
+
+
+function fetchAPI(word) {
+    wrapper.classList.remove("active")
+    var url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=vi&dt=t&q=${word}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => data(result, word))
+        .catch(err => err)
+}
